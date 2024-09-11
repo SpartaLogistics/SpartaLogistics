ef5cce7061e99d1827a41587f2f3fdb540d1b6ad
ParkJinWu <onu0311@gmail.com>
2024-09-11 11:51:05 +0900

    Feat : Hub 허브 정보 캐싱
    Issues: #22

--- com.sparta.logistics.client.hub/src/main/java/com/sparta/logistics/client/hub/controller/HubController.java
+++ com.sparta.logistics.client.hub/src/main/java/com/sparta/logistics/client/hub/controller/HubController.java
@@ -28,6 +28,8 @@ public class HubController {
         return ResponseEntity.ok(responseDto);
     }
 
+    // 허브 목록 조회 API
+    // TODO : 모든 조회 및 검색에서 is_delete가 false인 데이터만을 대상으로 처리
     @GetMapping
     public ResponseEntity<Page<HubResponseDto>> getAllHubs(
             @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
@@ -36,9 +38,10 @@ public class HubController {
 
 
     // 허브 단건 조회 API
-    @GetMapping("/{id}")
-    public ResponseEntity<HubResponseDto> getHubById(@PathVariable("id") UUID hubId) {
-        return hubService.getHubById(hubId);
+    @GetMapping("/{hubId}")
+    public ResponseEntity<HubResponseDto> getHubById(@PathVariable UUID hubId) {
+        HubResponseDto responseDto = hubService.getHubById(hubId);
+        return ResponseEntity.ok(responseDto);
     }
 
     //허브 수정 API
--- com.sparta.logistics.client.hub/src/main/java/com/sparta/logistics/client/hub/service/HubService.java
+++ com.sparta.logistics.client.hub/src/main/java/com/sparta/logistics/client/hub/service/HubService.java
@@ -7,12 +7,16 @@ import com.sparta.logistics.client.hub.model.Hub;
 import com.sparta.logistics.client.hub.repository.HubRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
@@ -22,6 +26,8 @@ public class HubService {
     private final HubRepository hubRepository;
 
     // 허브 생성 메서드
+    // 새로운 허브가 생성되면 전체 허브의 변화가 생겼기 때문에 삭제 필요
+    @CacheEvict(cacheNames = "hubAllCache", allEntries = true)
     public HubResponseDto createHub(HubRequestDto requestDto) {
         // DTO를 사용하여 Hub 객체 생성
         Hub hub = Hub.createHubInfoBuilder()
@@ -31,16 +37,22 @@ public class HubService {
         return HubResponseDto.of(hubRepository.save(hub));
     }
 
+    @Cacheable(cacheNames = "hubAllCache", key = "#pageable")
     public Page<HubResponseDto> getAllHubs(Pageable pageable) {
         return hubRepository.findAll(pageable).map(HubResponseDto::of);
     }
 
-    public ResponseEntity<HubResponseDto> getHubById(UUID hubId) {
+    @Cacheable(cacheNames = "hubCache", key = "#hubId")
+    public HubResponseDto getHubById(UUID hubId) {
         Hub hub = hubRepository.findById(hubId)
                 .orElseThrow(() -> new EntityNotFoundException("허브를 찾을 수 없습니다. ID: " + hubId));
-        return ResponseEntity.ok(HubResponseDto.of(hub));
+        return HubResponseDto.of(hub);
     }
 
+    // 허브 업데이트시 해당 캐시 수정
+    @CachePut(cacheNames = "hubCache", key = "#p0")
+    @CacheEvict(cacheNames = "hubAllCache", allEntries = true)
+    @Transactional
     public HubResponseDto updateHub(UUID hubID, HubRequestDto requestDto) {
         Hub hub = hubRepository.findById(hubID)
                 .orElseThrow(() -> new EntityNotFoundException("허브를 찾을 수 없습니다."));
@@ -50,10 +62,16 @@ public class HubService {
     }
 
     @Transactional
+    @CacheEvict(cacheNames = {"hubAllCache", "hubCache"}, key = "#hubId")
     public void deleteHub(UUID hubId) {
         Hub hub = hubRepository.findById(hubId)
                 .orElseThrow(() -> new EntityNotFoundException("허브를 찾을 수 없습니다. ID: " + hubId));
         hub.softDelete();
         hubRepository.save(hub);
+    }
+
+    //TODO : is_deleted false인것만 조회
+    public Hub findHubById(UUID hubId) {
+        return hubRepository.findById(hubId).orElseThrow(NoSuchElementException::new);
     }
 }
