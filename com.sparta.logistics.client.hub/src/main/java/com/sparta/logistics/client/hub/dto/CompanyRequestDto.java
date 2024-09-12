fbc66f1c425cda0156bba47a1b11070c47616ac5
ParkJinWu <onu0311@gmail.com>
2024-09-12 17:48:07 +0900

    Feat : RequestBody 바인딩 에러 처리
    Issues: #41

--- com.sparta.logistics.client.hub/src/main/java/com/sparta/logistics/client/hub/dto/CompanyRequestDto.java
+++ com.sparta.logistics.client.hub/src/main/java/com/sparta/logistics/client/hub/dto/CompanyRequestDto.java
@@ -1,6 +1,8 @@
 package com.sparta.logistics.client.hub.dto;
 
 import com.sparta.logistics.client.hub.enums.CompanyType;
+import com.sparta.logistics.client.hub.model.validation.CompanyVaild0001;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,9 +15,25 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CompanyRequestDto {
+
+    @NotNull(groups = {CompanyVaild0001.class},
+            message = "업체명 정보가 누락되었습니다."
+    )
     private String name;
     //TODO : userID
+
+    @NotNull(groups = {CompanyVaild0001.class},
+            message = "업체타입 정보가 누락되었습니다."
+    )
     private CompanyType companyType;
+
+    @NotNull(groups = {CompanyVaild0001.class},
+            message = "업체 주소 정보가 누락되었습니다."
+    )
     private String address;
+
+    @NotNull(groups = {CompanyVaild0001.class},
+            message = "업체 관리 허브 ID 정보가 누락되었습니다."
+    )
     private UUID managingHubId;
 }
